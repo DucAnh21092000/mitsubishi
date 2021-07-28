9435696d68512187db40856e7f39cf2351c30ef2
DucAnh21092000 <dinhanh21092000@gmail.com>
2021-07-28 16:09:34 +0700

    duc anhh

--- js/switchImage.js
+++ js/switchImage.js
@@ -1,21 +1,3 @@
-(
-    function() {
-        let headerItem = document.querySelector('.header-menu__item:nth-child(2)');
-        let hideMenu = document.querySelector('.hide-menu')
-        headerItem.onmouseover = function() {
-            hideMenu.style = `display:block`;
-            headerItem.onmouseout = function(e) {
-                e.preventDefault();
-            }
-            hideMenu.onmouseover = function() {
-                hideMenu.style = `display:block`;
-            }
-            hideMenu.onmouseout = function() {
-                hideMenu.style = `display:none`;
-            }
-        }
-    }
-)();
 //switch image
 var arrImage = [{
         "url ": "1. jpg ",
